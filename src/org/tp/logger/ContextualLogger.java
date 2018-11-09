1d2c6c07518fcce8c9f5b4313eecfcfeb8bf61c8
toto5100 <toto5100@users.noreply.github.com>
2018-11-09 11:47:29 +0100

    Ajout du logger (non terminé)

---
+++ src/org/tp/logger/ContextualLogger.java
@@ -0,0 +1,23 @@
+package org.tp.logger;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class ContextualLogger implements Logger {
+
+    private Logger delegateLogger;
+    private Object callerClass;
+    private final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    public ContextualLogger(Logger delegateLogger, Object callerClass){
+        this.delegateLogger = delegateLogger;
+        this.callerClass = callerClass;
+    }
+
+    @Override
+    public void log(String message)
+    {
+        delegateLogger.log( sdf.format( new Date() ) + " " + callerClass + " " + message );
+    }
+
+}
---
+++ src/org/tp/logger/LoggerFactory.java
@@ -0,0 +1,10 @@
+package org.tp.logger;
+
+public class LoggerFactory {
+
+public static Logger getLogger(String name)
+{
+return new ConsoleLogger();
+}
+
+}
