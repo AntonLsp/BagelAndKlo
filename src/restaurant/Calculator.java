c3b3ea324757ae86f29de7b52e618d16aa15e91b
MarkoKlo <klobucarmarko1@gmail.com>
2018-11-26 09:57:30 +0100

    Correction Calculator

--- src/restaurant/Calculator.java
+++ src/restaurant/Calculator.java
@@ -14,19 +14,13 @@ public class Calculator {
 
     public float getPriceTtc(List<Product> products,float TVA){
         float priceTtc=0;
-        for(int i=0;i<products.size();i++){
-            priceTtc= priceTtc + products.get(i).getPrice();
-        }
-        priceTtc = (priceTtc * (1+TVA));
+        priceTtc=getPriceHt(products)*(1+TVA);
         return priceTtc;
     }
 
-    public float getPriceDiscount(List<Product> products, float reduction){
+    public float getPriceDiscount(List<Product> products, float reduction, float TVA){
         float priceDiscount=0;
-        for(int i=0;i<products.size();i++){
-            priceDiscount= priceDiscount + products.get(i).getPrice();
-        }
-        priceDiscount = (priceDiscount * (1-reduction));
+        priceDiscount=getPriceTtc(products,TVA)*(1-reduction);
         return priceDiscount;
     }
 }
